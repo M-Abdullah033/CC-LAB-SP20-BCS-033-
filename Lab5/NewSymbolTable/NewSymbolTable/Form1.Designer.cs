c91c4c8284c370777941b3005d13c516271120b3
M-Abdullah033 <113822293+M-Abdullah033@users.noreply.github.com>
2023-10-05 15:58:34 +0500

    Add files via upload

---
+++ Lab5/NewSymbolTable/NewSymbolTable/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace NewSymbolTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.addbtn = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 224);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(147, 85);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(673, 224);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(147, 85);
+            this.textBox2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(290, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // addbtn
+            // 
+            this.addbtn.Location = new System.Drawing.Point(228, 332);
+            this.addbtn.Name = "addbtn";
+            this.addbtn.Size = new System.Drawing.Size(86, 43);
+            this.addbtn.TabIndex = 3;
+            this.addbtn.Text = "Add";
+            this.addbtn.UseVisualStyleBackColor = true;
+            this.addbtn.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(496, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 402);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.addbtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button addbtn;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Lab5/NewSymbolTable/NewSymbolTable/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewSymbolTable
+{
+    public partial class Form1 : Form
+    {
+
+        private Dictionary<string, string> symbolTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            symbolTable = new Dictionary<string, string>();
+            InitializeDataGridView();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void InitializeDataGridView()
+        {
+            dataGridView1.Columns.Add("Key", "Key");
+            dataGridView1.Columns.Add("Value", "Value");
+        }
+
+       
+
+        private void UpdateDataGridView()
+        {
+            dataGridView1.Rows.Clear();
+
+            foreach (var pair in symbolTable)
+            {
+                dataGridView1.Rows.Add(pair.Key, pair.Value);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)  //Add Button
+        {
+            string key = textBox1.Text;
+            string value = textBox2.Text;
+
+            if (string.IsNullOrEmpty(key))
+            {
+                MessageBox.Show("Key cannot be empty.");
+            }
+            else if (symbolTable.ContainsKey(key))
+            {
+                MessageBox.Show("Key already exists.");
+            }
+            else
+            {
+                symbolTable[key] = value;
+                textBox1.Clear();
+                textBox2.Clear();
+                UpdateDataGridView();
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e) //Search Button
+        {
+            string key = textBox1.Text;
+
+            if (string.IsNullOrEmpty(key))
+            {
+                MessageBox.Show("Key cannot be empty.");
+            }
+            else if (symbolTable.ContainsKey(key))
+            {
+                string value = symbolTable[key];
+                MessageBox.Show(string.Format("Value for key '{0}': {1}", key, value));
+
+            }
+            else
+            {
+                MessageBox.Show("Key not found.");
+            }
+        }
+    }
+}
+
