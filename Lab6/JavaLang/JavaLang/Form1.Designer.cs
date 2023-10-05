da83f65baadc8d291b94fb674cb0ac0b5c4572b3
M-Abdullah033 <113822293+M-Abdullah033@users.noreply.github.com>
2023-10-05 15:59:54 +0500

    Add files via upload

---
+++ Lab6/JavaLang/JavaLang/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace JavaLang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GrammarTextBox = new System.Windows.Forms.TextBox();
+            this.ParseButton = new System.Windows.Forms.Button();
+            this.ResultRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // GrammarTextBox
+            // 
+            this.GrammarTextBox.Location = new System.Drawing.Point(135, 23);
+            this.GrammarTextBox.Multiline = true;
+            this.GrammarTextBox.Name = "GrammarTextBox";
+            this.GrammarTextBox.Size = new System.Drawing.Size(384, 97);
+            this.GrammarTextBox.TabIndex = 0;
+            // 
+            // ParseButton
+            // 
+            this.ParseButton.Location = new System.Drawing.Point(256, 162);
+            this.ParseButton.Name = "ParseButton";
+            this.ParseButton.Size = new System.Drawing.Size(110, 51);
+            this.ParseButton.TabIndex = 1;
+            this.ParseButton.Text = "Parse";
+            this.ParseButton.UseVisualStyleBackColor = true;
+            this.ParseButton.Click += new System.EventHandler(this.ParseButton_Click);
+            // 
+            // ResultRichTextBox
+            // 
+            this.ResultRichTextBox.Location = new System.Drawing.Point(135, 241);
+            this.ResultRichTextBox.Name = "ResultRichTextBox";
+            this.ResultRichTextBox.Size = new System.Drawing.Size(384, 133);
+            this.ResultRichTextBox.TabIndex = 2;
+            this.ResultRichTextBox.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 401);
+            this.Controls.Add(this.ResultRichTextBox);
+            this.Controls.Add(this.ParseButton);
+            this.Controls.Add(this.GrammarTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox GrammarTextBox;
+        private System.Windows.Forms.Button ParseButton;
+        private System.Windows.Forms.RichTextBox ResultRichTextBox;
+    }
+}
+
---
+++ Lab6/JavaLang/JavaLang/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JavaLang
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<string>> productionRules = new Dictionary<string, List<string>>();
+        private Dictionary<string, HashSet<string>> firstSets = new Dictionary<string, HashSet<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseButton_Click(object sender, EventArgs e)
+        {
+            firstSets.Clear();
+            ResultRichTextBox.Clear();
+
+            // Parse grammar and calculate First Sets
+            ParseGrammar();
+            CalculateFirstSets();
+
+            // Display First Sets in the ResultRichTextBox
+            foreach (var nonTerminal in firstSets.Keys)
+            {
+                var firstSet = string.Join(", ", firstSets[nonTerminal]);
+                ResultRichTextBox.AppendText(string.Format("First({0}) = {{{1}}}\n", nonTerminal, firstSet));
+
+            }
+        }
+         private void ParseGrammar()
+        {
+            productionRules.Clear();
+            var rules = GrammarTextBox.Lines;
+
+            foreach (var rule in rules)
+            {
+                var parts = rule.Split(new[] { "->" }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length == 2)
+                {
+                    var nonTerminal = parts[0].Trim();
+                    var productions = parts[1].Trim().Split('|').Select(p => p.Trim()).ToList();
+                    productionRules[nonTerminal] = productions;
+                }
+            }
+        }
+
+        private void CalculateFirstSets()
+        {
+            foreach (var nonTerminal in productionRules.Keys)
+            {
+                CalculateFirstSet(nonTerminal);
+            }
+        }
+
+        private void CalculateFirstSet(string nonTerminal)
+        {
+            if (!firstSets.ContainsKey(nonTerminal))
+            {
+                firstSets[nonTerminal] = new HashSet<string>();
+            }
+
+            foreach (var production in productionRules[nonTerminal])
+            {
+                var symbols = production.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (symbols.Length == 0)
+                {
+                    // Empty production
+                    firstSets[nonTerminal].Add("");
+                }
+                else if (char.IsUpper(symbols[0][0]))
+                {
+                    // Non-terminal symbol
+                    CalculateFirstSet(symbols[0]);
+                    firstSets[nonTerminal].UnionWith(firstSets[symbols[0]]);
+                }
+                else
+                {
+                    // Terminal symbol
+                    firstSets[nonTerminal].Add(symbols[0]);
+                }
+            }
+        }
+    }
+}
